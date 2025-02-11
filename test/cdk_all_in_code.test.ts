a77ab8f3f34b74edc7a2f4fc205b63d9b3a70c48
Yudai Jinno <jinno.yudai@classmethod.jp>
2025-02-11 17:35:16 +0900

    Initial commit

---
+++ test/cdk_all_in_code.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkAllInCode from '../lib/cdk_all_in_code-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_all_in_code-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkAllInCode.CdkAllInCodeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
